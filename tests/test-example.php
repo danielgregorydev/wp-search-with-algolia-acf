49751ce1c9b617fac8bde2ff01d82fcd950d7a50
Daniel Gregory <danny@danielgregory.dev>
2022-06-23 11:49:14 +0100

    remove unneeded files

--- tests/test-example.php
+++ tests/test-example.php
@@ -6,9 +6,9 @@
  * @package Custom_Membership_Id
  */
 
-use Dgd\ExamplePlugin\App\Models\Member;
+use Dgd\AlgoliaAcf\App\Models\Member;
 
-use function Dgd\ExamplePlugin\customMemberId;
+use function Dgd\AlgoliaAcf\customMemberId;
 
 class Test_Example extends WP_UnitTestCase
 {
